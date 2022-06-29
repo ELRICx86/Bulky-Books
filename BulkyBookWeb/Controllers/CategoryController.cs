56ab651e46d348dd775228d9692ff5d107c5523e
ELRICx86 <sajidul.saif.71@gmail.com>
2022-06-29 18:55:23 +0600

    Category Controller added and bootswatch added

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDBcontext _db;
+
+        public CategoryController(AppDBcontext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category>  objCategoryList = _db.Categories;
+            return View();
+        }
+    }
+}
